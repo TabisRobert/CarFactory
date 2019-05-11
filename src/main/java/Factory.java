2f716e565dae1934c26dc36eaa7259cb385bcc1c
Robert Tabiś <robert.tabis@hotmail.com>
2019-05-11 13:40:33 +0200

    pierwszy samochód w naszej fabryce

---
+++ src/main/java/Factory.java
@@ -0,0 +1,19 @@
+public class Factory {
+    private int number;
+    private int price;
+    private String owner;
+
+    public Factory(int number, int price, String owner) {
+        this.number = number;
+        this.price = price;
+        this.owner = owner;
+    }
+
+    public static void main(String[] args) {
+        Factory car = new Factory(123,321,"Adriam");
+        System.out.println(car);
+    }
+    public String toString(){
+        return "Nr rejestracyjny  "+this.number +" cena: "+this.price+" właściciel: "+this.owner;
+    }
+}
