f85133430c20088bf82063eedf8ac2c160a7b21d
Lukasz Fijol <l.fijol@gmail.com>
2019-05-11 15:58:01 +0200

    www

--- src/main/java/Factory.java
+++ src/main/java/Factory.java
@@ -1,14 +1,10 @@
 public class Factory {
 
-<<<<<<< HEAD
     public Dealer dealer;
 
     public static void main(String[] args) {
         Dealer sell = new Dealer(122344);
         Producer car = new Producer(12345);
-=======
-    public static void main(String[] args) {
->>>>>>> 2c6e58192bd57d622cd545827ed8074f677a1a10
 
     }
 
