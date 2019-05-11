156b448688a40a0050cdbfda5f4a258200003017
Lukasz Fijol <l.fijol@gmail.com>
2019-05-11 14:38:23 +0200

    Producer

---
+++ src/main/java/Producer.java
@@ -0,0 +1,13 @@
+public class Producer {
+
+    String colour;
+    int maxSpeed;
+    String type;
+
+    public Producer(String colour, int maxSpeed, String type) {
+        this.colour = colour;
+        this.maxSpeed = maxSpeed;
+        this.type = type;
+    }
+        
+}
